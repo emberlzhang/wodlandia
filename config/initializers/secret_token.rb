fbd968088fd1b4ccab9e4018753d88382f8c8078
janetchang <janetlaichang@gmail.com>
2013-07-06 16:18:40 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wodlandia::Application.config.secret_key_base = '8311eac409508ec013471f5218d94f39ab2f9fccfb7f322cdef4408f291d2835dd4ace06e4d3774d408a7ab79c1d855ab2358d86c794f341b3b2a92cd0b4cda8'
