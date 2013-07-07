e910151ce96bf1138250ef441436b27f72868a42
Chris Faulkner <thefaulkner@gmail.com>
2013-07-06 20:11:47 -0700

    Add auth controller for human/api.

--- lib/humanapi.rb
+++ lib/humanapi.rb
@@ -1,5 +1,9 @@
 require 'oauth2'
 
 def client
-  OAuth2::Client.new(ENV['HUMANAPI_CLIENT_ID'], ENV['HUMANAPI_CLIENT_SECRET'], site: ENV['HUMANAPI_SITE'] || 'https://user.humanapi.co')
+  OAuth2::Client.new(ENV['HUMANAPI_CLIENT_ID'],
+                     ENV['HUMANAPI_CLIENT_SECRET'],
+                     site: 'https://api.humanapi.co/v1/human/',
+                     token_url: 'https://user.humanapi.co/oauth/token',
+                     authorize_url: 'https://user.humanapi.co/oauth/authorize')
 end
